be55e65908496d75bf081f6c66ce343acdc83bd5
bensonc <craig.benson@ppb.com>
2024-08-18 14:26:52 +0100

    removing caching

--- release_manager/src/main/kotlin/com/release_manager/ReleaseManagerApplication.kt
+++ release_manager/src/main/kotlin/com/release_manager/ReleaseManagerApplication.kt
@@ -2,10 +2,8 @@ package com.release_manager
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cache.annotation.EnableCaching
 
 @SpringBootApplication
-@EnableCaching
 class ReleaseManagerApplication
 
 fun main(args: Array<String>) {
