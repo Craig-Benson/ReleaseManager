7a8254e419fe8f7972c2472c227e9084abcff330
bensonc <craig.benson@ppb.com>
2024-08-18 14:00:27 +0100

    cleaning todos

--- release_manager/src/main/kotlin/com/release_manager/util/ServiceMapper.kt
+++ release_manager/src/main/kotlin/com/release_manager/util/ServiceMapper.kt
@@ -11,7 +11,6 @@ import java.time.LocalDateTime
 @Slf4j
 class ServiceMapper {
 
-    //todo test
     fun mapServices(
         systemVersionNumber: Int,
         currentlyDeployedServices: List<InternalService>,
